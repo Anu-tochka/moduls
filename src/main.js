0abb71c7ea52ea7b4f2910803dafb5664c12de17
Anu-tochka <anuta-zh@mail.ru>
2021-04-23 19:53:39 +0300

    start

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import { diffDates, diffToHtml } from "./datecalc.js"; // 1
+﻿import { diffDates, diffToHtml } from "./datecalc.js"; // 1
 import { formatError } from "./utils.js"; // 2
 
 const dateCalcForm = document.getElementById("datecalc");
 const dateCalcResult = document.getElementById("datecalc__result");
+const switch = document.getElementById("switch ");
+const dateCalcResult = document.getElementById("timer__result");
 
 dateCalcForm.addEventListener("submit", handleCalcDates);
 
@@ -14,8 +16,39 @@ function handleCalcDates(event) {
     firstDate = firstDate.value, secondDate = secondDate.value;
 
     if (firstDate && secondDate) {
-        const diff = diffDates(firstDate, secondDate); // 3
-        dateCalcResult.innerHTML = diffToHtml(diff); // 4
+        const diff = diffDates(firstDate, secondDate); 
+        dateCalcResult.innerHTML = diffToHtml(diff); 
     }
-	 else dateCalcResult.innerHTML = formatError("Для расчета промежутка необходимо заполнить оба поля"); // 5
+	 else dateCalcResult.innerHTML = formatError("Для расчета промежутка необходимо заполнить оба поля"); 
+}
+
+switch.addEventListener("click", handleSwitch);
+
+function handleSwitch(event) {
+    dateCalcResult.innerHTML = "";
+    event.preventDefault();
+
+    let { firstDate, secondDate } = event.target.elements;
+    firstDate = firstDate.value, secondDate = secondDate.value;
+
+    if (firstDate && secondDate) {
+        const diff = diffDates(firstDate, secondDate); 
+        dateCalcResult.innerHTML = diffToHtml(diff); 
+    }
+	 else dateCalcResult.innerHTML = formatError("Для расчета промежутка необходимо заполнить оба поля"); 
+}
+switch.addEventListener("click", handleTimer);
+
+function handleTimer(event) {
+    dateCalcResult.innerHTML = "";
+    event.preventDefault();
+
+    let { firstDate, secondDate } = event.target.elements;
+    firstDate = firstDate.value, secondDate = secondDate.value;
+
+    if (firstDate && secondDate) {
+        const diff = diffDates(firstDate, secondDate); 
+        dateCalcResult.innerHTML = diffToHtml(diff); 
+    }
+	 else dateCalcResult.innerHTML = formatError("Для расчета промежутка необходимо заполнить оба поля"); 
 }
