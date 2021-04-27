77ede1625861c7e16addf7ee590af0f598717e0a
Anu-tochka <anuta-zh@mail.ru>
2021-04-27 05:58:46 +0300

    module 1

--- src/main.js
+++ src/main.js
@@ -3,17 +3,19 @@ import { formatError } from "./utils.js"; // 2
 
 const dateCalcForm = document.getElementById("datecalc");
 const dateCalcResult = document.getElementById("datecalc__result");
-const switch = document.getElementById("switch ");
-const dateCalcResult = document.getElementById("timer__result");
+const switcher = document.getElementById("switch ");
+const timerStart = document.getElementById("start");
+const timerStop = document.getElementById("stop");
+const timeShow = document.getElementById("timer__show");
+
+let { firstDate, secondDate, time } = event.target.elements;
+firstDate = firstDate.value, secondDate = secondDate.value, time = time.value;
 
 dateCalcForm.addEventListener("submit", handleCalcDates);
 
 function handleCalcDates(event) {
     dateCalcResult.innerHTML = "";
     event.preventDefault();
-
-    let { firstDate, secondDate } = event.target.elements;
-    firstDate = firstDate.value, secondDate = secondDate.value;
 
     if (firstDate && secondDate) {
         const diff = diffDates(firstDate, secondDate); 
@@ -22,7 +24,7 @@ function handleCalcDates(event) {
 	 else dateCalcResult.innerHTML = formatError("Для расчета промежутка необходимо заполнить оба поля"); 
 }
 
-switch.addEventListener("click", handleSwitch);
+switcher.addEventListener("click", handleSwitch);
 
 function handleSwitch(event) {
     dateCalcResult.innerHTML = "";
@@ -37,18 +39,20 @@ function handleSwitch(event) {
     }
 	 else dateCalcResult.innerHTML = formatError("Для расчета промежутка необходимо заполнить оба поля"); 
 }
-switch.addEventListener("click", handleTimer);
 
-function handleTimer(event) {
-    dateCalcResult.innerHTML = "";
-    event.preventDefault();
+timerStart.addEventListener("click", setInterval(startTime, 1000));
 
-    let { firstDate, secondDate } = event.target.elements;
-    firstDate = firstDate.value, secondDate = secondDate.value;
-
-    if (firstDate && secondDate) {
-        const diff = diffDates(firstDate, secondDate); 
-        dateCalcResult.innerHTML = diffToHtml(diff); 
+function startTime() {
+    if (time <= 0) {
+        clearInterval();
+        var sound = new Howl({
+		  src: ['sound.mp3']
+		});
+        sound.play();
+    } else { // Иначе
+        timerShow.innerHTML = time;
     }
-	 else dateCalcResult.innerHTML = formatError("Для расчета промежутка необходимо заполнить оба поля"); 
+    --time;
 }
+
+timerStop.addEventListener("click", clearInterval());
